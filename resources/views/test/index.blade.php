ea1af02d8b7bc00b83aa645d9151841364ae06c2
htn9sys <hatsune.927@gmail.com>
2023-06-20 22:13:11 +0900

    testのビュー、コントローラー、モデル作成。ルーティングをtest/indexへ設定

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test View</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            border: 1px solid #ccc;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            color: #333;
+            text-align: center;
+        }
+
+        p {
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to the Test View!</h1>
+        <p>This is a sample test view for your mini app.</p>
+        <p>Feel free to customize this view to fit your needs.</p>
+        <p>You can add more HTML elements and apply CSS styles as desired.</p>
+    </div>
+</body>
+</html>
